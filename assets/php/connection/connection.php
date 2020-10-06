31ca021a3215a7a7425545d896f53d8775d04ef2
whavafet09 <72425004+whavafet09@users.noreply.github.com>
2020-10-06 21:28:18 +0800

    Update connection.php

--- assets/php/connection/connection.php
+++ assets/php/connection/connection.php
@@ -3,7 +3,7 @@ require_once ("meekrodb/db.class.php");
 
 
 
-	$conn = new MeekroDB("localhost", "root", "", "magigilas");
+	$conn = new MeekroDB(" remotemysql.com", "Eq5CRCTAMV", "sQrDQ9jHj2", "Eq5CRCTAMV");
 
 
 
